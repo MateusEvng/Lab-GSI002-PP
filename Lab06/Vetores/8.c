af48a3eb2007e46f221bc42b41956ae5083e38a1
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2022-06-15 10:37:31 -0300

    Add files via upload

--- Lab06/Vetores/8.c
+++ Lab06/Vetores/8.c
@@ -3,6 +3,34 @@
 int main(void) {
 
   float array[10];
-  //terminar em casa
+  int i = 0; //contador 
+  int j = 0; //verificador terá j if's
+
+  //percorre o vetor
+  while (i<10)
+  {
+    printf("Insira um numero para array[%d]: \n", i);
+    scanf("%f", &array[i]);
+    
+    //verifica o vetor se algum i ja foi inserido.
+    for (j; j < i; j++)
+    {
+       while ((array[i] == array[j])){
+        printf("Este numero ja foi inserido. Insira outro numero: \n\n");
+        printf("Insira um numero para array[%d]: \n", i);
+        scanf("%f", &array[i]);
+       }
+    }
+
+    i++;
+  }
+  
+  //mostra o vetor
+  for (int print = 0; print < 10; print++)
+  {
+    printf("%.2f ",array[print]);
+  }
+  
+
   return 0;
 }
